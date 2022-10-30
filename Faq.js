17aa0ab3902d424bb5914937e0e6f4e1615786ef
Muktha-Poduval <89790731+Muktha-Poduval@users.noreply.github.com>
2022-10-30 16:12:30 +0800

    Update Faq.js

--- Faq.js
+++ Faq.js
@@ -1,3 +1,40 @@
+<!doctype html>
+<html>
+<!--**********Head***********-->
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<style>body{margin:0;}</style>
+
+
+
+<title>
+Games
+</title>
+</head>
+<!--**********Body***********-->
+<body>
+<!--**********Header***********-->
+<header>
+<!--**********Nav***********-->
+  
+ <div style="background:#ABC4FF; padding:10px;">
+<ul style="word-spacing:40px;">
+<li style="display:inline-block;"><a href="index.html" style="background:#D7E3FC; padding:8px 15px; text-decoration:none; border-radius:20px;">Home </a></li>
+<li style="display:inline-block;"><a href="hobby.html" style="background:#D7E3FC; padding:8px 15px; text-decoration:none; border-radius:20px;">Hobby </a></li>
+<li style="display:inline-block;"><a href="contacts.html" style="background:#D7E3FC; padding:8px 15px; text-decoration:none; border-radius:20px;">Contact</a></li>
+<li style="display:inline-block;"><a href="feedback.html" style="background:#D7E3FC; padding:8px 15px; text-decoration:none; border-radius:20px;">Feedback</a></li>
+</ul>
+</div>
+
+<div style="background:#C1D3FE; padding:20px; text-align:center;">
+<h1>FAQ page</h1>
+</div>
+
+</header>
+
+<!--**********Main***********-->
+<main style="background:#D7E3FC; padding:20px;">
+
 $(function() {
 
   $('#login-show').click(function() {
@@ -24,3 +61,14 @@ $(function() {
     }
   );
 });
+
+</main>
+
+<!--**********Footer***********-->
+<footer style="background:#ABC4FF; padding:20px; text-align:center;">
+<h2>Let's Keep In Touch</h2>
+<a href="mailto:muktha.pod@gmail.com" style="background:#D7E3FC; padding:10px 20px; border-radius:5px; text-decoration:none; ">Connect</a>
+</footer>
+<script src="script.js"></script>
+</body>
+</html>
